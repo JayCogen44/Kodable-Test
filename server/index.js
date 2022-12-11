0762b872fcdcfef487e8ae3f2eed1479f06fe57e
James Cogen <jamescogen@jamess-air.lan>
2022-12-11 15:44:00 -0700

    broke out routes

--- server/index.js
+++ server/index.js
@@ -1,28 +1,15 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
-const initDB = require('./controllers/initDB');
+const routes = require('./routes');
 
+//
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get(
-  '/api/initialize-data',
-  initDB.createPizzaAnalyticsTable,
-  initDB.populateFromCsv,
-  (req, res) => {
-    res.send('db connected');
-  }
-);
-
-app.get('/api', (req, res) => {
-  res.send('<h1>Welcome to the api</h1>');
-});
-
-app.get('/', (req, res) => {
-  res.send('<h1>Pizza Analytics</h1>');
-});
+//routes
+routes.initialize(app);
 
 app.listen(process.env.PORT, function () {
   console.log(`server running: listening on port ${process.env.PORT}`);
